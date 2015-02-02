06028fb835026cd3018eba40de1933dee84ff9ff
Giniouxe <aurelie.ginioux@gmail.com>
2015-02-02 20:55:13 +0100

    Transfer Solicom

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Returns full title per page.
   def full_title(page_title = '')
-    base_title = 'Sample App'
+    base_title = 'Solicom | Agence de communication'
     page_title.empty? ? base_title : "#{page_title} | #{base_title}"
   end
 end
