810ba497e2e1d8caa30974fc0b294ab0d2c2da9e
Giniouxe <aurelie.ginioux@gmail.com>
2015-02-03 17:37:03 +0100

    Change tests to match deletion of help and about pages

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -5,12 +5,12 @@ class PagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Sample App"
+    assert_select "title", 'Solicom | Agence de communication'
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact | Sample App"
+    assert_select "title", 'Contact | Solicom | Agence de communication'
   end
 end
--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
   test "full_title helper" do
-    assert_equal full_title(''),        "Sample App"
-    assert_equal full_title("Sign Up"), "Sign Up | Sample App"
+    assert_equal full_title(''),        'Solicom | Agence de communication'
+    assert_equal full_title("Sign Up"), "Sign Up | Solicom | Agence de communication"
   end
 end
