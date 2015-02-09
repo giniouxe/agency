c24bfe12162bb8f01c7d52e9573b9f132f430fe2
Giniouxe <aurelie.ginioux@gmail.com>
2015-02-09 18:51:05 +0100

    Create sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root            'pages#home'
 
   get 'contact'   => "pages#contact"
   get 'signup'    => "users#new"
+
+  get 'login'     => "sessions#new"
+  post 'login'    => "sessions#create"
+  get 'logout'    => "sessions#destroy  "
 
   resources :users
 end
