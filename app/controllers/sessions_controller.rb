e94ed07c4552f0c345f9685d6fecb7bbbf8ada90
Giniouxe <aurelie.ginioux@gmail.com>
2015-02-20 18:27:30 +0100

    Handle invalid login information

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:sessions][:email].downcase)
+    if user && user.authenticate(params[:sessions][:password])
+      #REDIRECT
+    else
+      flash.now[:danger] = 'Invalid user/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
