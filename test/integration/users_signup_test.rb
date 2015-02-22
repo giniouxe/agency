f98da2237f5e5447c6cd4c05bebd1d4326a79c8e
Giniouxe <aurelie.ginioux@gmail.com>
2015-02-22 21:47:17 +0100

    Saved new user is already logged in

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,8 +4,11 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "user shouldn't be saved if invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, user: {name: "", email: "foo@invalid",
-                              password: "foobar", password_confirmation: "foo"}
+      post users_path, user: {
+        name: '',
+        email: 'foo@invalid',
+        password: 'foobar',
+        password_confirmation: 'foo' }
     end
     assert_template 'users/new'
   end
@@ -13,10 +16,11 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "user should be saved if valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post_via_redirect users_path, user: {name: "Foo Bar",
-                                          email: "foobar@example.com",
-                                          password: "foobar",
-                                          password_confirmation: "foobar"}
+      post_via_redirect users_path, user: {
+        name: 'Foo Bar',
+        email: 'foobar@example.com',
+        password: 'foobar',
+        password_confirmation: 'foobar' }
     end
     assert_template 'users/show'
   end
@@ -24,24 +28,29 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "should display error messages" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, user: {name: "", email: "foo@invalid",
-        password: "foobar", password_confirmation: "foo"}
+      post users_path, user: {
+        name: '',
+        email: 'foo@invalid',
+        password: 'foobar',
+        password_confirmation: 'foo' }
       end
       assert_template 'users/new'
-      assert_select "div", :attributes => {:id => 'error-explaination'}
-      assert_select "div", :attributes => {:class => 'field_with_error'}
+      assert_select 'div', :attributes => { :id => 'error-explaination' }
+      assert_select 'div', :attributes => { :class => 'field_with_error' }
   end
 
-  test "welcome flash if user saved" do
+  test "friendly welcome flash if user saved" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post_via_redirect users_path, user: {name: "Foo Bar",
-        email: "foobar@example.com",
-        password: "foobar",
-        password_confirmation: "foobar"}
+      post_via_redirect users_path, user: {
+        name: 'Foo Bar',
+        email: 'foobar@example.com',
+        password: 'foobar',
+        password_confirmation: 'foobar' }
       end
       assert_template 'users/show'
+      assert is_logged_in?, "User is not logged in"
       assert_not flash.nil?
-      assert_select "div", :attributes => {class: "alert alert-succes", content: "Bienvenue chez Solicom"}
+      assert_select 'div', :attributes => { class: 'alert alert-succes', content: 'Bienvenue chez Solicom' }
     end
 end
