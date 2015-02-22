84cd58776e3b210f16c994219e0526b430597822
Giniouxe <aurelie.ginioux@gmail.com>
2015-02-22 21:28:55 +0100

    Add valid informations login test

--- test/fabricators/user_fabricator.rb
+++ test/fabricators/user_fabricator.rb
@@ -1,5 +1,2 @@
 Fabricator(:user) do
-  name { 'Charlie' }
-  email { 'charlie@example.com' }
-  password_digest { User.digest('croquettes') }
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,13 +1,32 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  test "wrong email/password combination" do
+  def setup
+      @user = Fabricate(:user,
+                        name: 'Charlie',
+                        email: 'charlie@example.com',
+                        password: 'croquettes')
+  end
+
+  test "login with invalid informations" do
     get login_path
     assert_template 'sessions/new'
-    post login_path, sessions: { email: 'wrong', password: 'wrong' }
+    post login_path, sessions: { email: @user.email, password: 'nocroquettes' }
     assert_template 'sessions/new'
     assert_not flash.empty?, "flash don't appear"
     get root_path
     assert flash.empty?, "flash not empty"
+  end
+
+  test "login with valid informations" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, sessions: { email: @user.email, password: 'croquettes' }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end
