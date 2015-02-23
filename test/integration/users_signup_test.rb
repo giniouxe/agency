36f63f3f30f395aeb5fb60612faa812a6ebc20b4
Giniouxe <aurelie.ginioux@gmail.com>
2015-02-23 23:35:58 +0100

    Tests refactoring

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,31 +1,7 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  test "user shouldn't be saved if invalid signup information" do
-    get signup_path
-    assert_no_difference 'User.count' do
-      post users_path, user: {
-        name: '',
-        email: 'foo@invalid',
-        password: 'foobar',
-        password_confirmation: 'foo' }
-    end
-    assert_template 'users/new'
-  end
-
-  test "user should be saved if valid signup information" do
-    get signup_path
-    assert_difference 'User.count', 1 do
-      post_via_redirect users_path, user: {
-        name: 'Foo Bar',
-        email: 'foobar@example.com',
-        password: 'foobar',
-        password_confirmation: 'foobar' }
-    end
-    assert_template 'users/show'
-  end
-
-  test "should display error messages" do
+  test "user should not be saved if invalid signup informations" do
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, user: {
@@ -39,7 +15,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       assert_select 'div', :attributes => { :class => 'field_with_error' }
   end
 
-  test "friendly welcome flash if user saved" do
+  test "valid sign up informations" do
     get signup_path
     assert_difference 'User.count', 1 do
       post_via_redirect users_path, user: {
