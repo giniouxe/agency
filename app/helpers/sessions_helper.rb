0debb007e4b99a1081974356eeb50d03a5ffc623
Giniouxe <aurelie.ginioux@gmail.com>
2015-03-29 17:25:21 +0200

    Move current user method to a more pertinent place

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,18 +3,6 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  def current_user
-    if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: session[:user_id])
-    elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
-    end
-  end
-
   def logged_in?
     !current_user.nil?
   end
@@ -29,6 +17,18 @@ module SessionsHelper
     user.forget
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
+  end
+
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id])
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   def log_out
