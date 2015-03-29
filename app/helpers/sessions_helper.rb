4da1b5590e4d88e3feb22486cb13631aae2872b3
Giniouxe <aurelie.ginioux@gmail.com>
2015-03-29 16:27:04 +0200

    Correct forget user method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,16 +22,17 @@ module SessionsHelper
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent.signed[:remember_token] = user.remember_token
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
   def forget(user)
-    forget(current_user)
+    user.forget
     cookies.delete(:user_id)
-    cookies.delete(:remeber_token)
+    cookies.delete(:remember_token)
   end
 
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
