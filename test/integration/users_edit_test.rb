481e62ecfb23847f001c6cb6a7957d55b276bd8f
Giniouxe <aurelie.ginioux@gmail.com>
2015-03-29 23:10:12 +0200

    User can update profile informations

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,41 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = Fabricate(
+    :user,
+    name: 'Charlie',
+    email: 'charlie@example.com',
+    password: 'croquettes')
+  end
+
+  test 'unsuccessful edit' do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), user: {
+      name: '',
+      email: 'invalid',
+      password: 'foo',
+      password_confirmation: 'bar'
+    }
+    assert_template 'users/edit'
+  end
+
+  test 'successful edit' do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    new_name = 'Charlie The Cat'
+    new_email = 'thecat@example.com'
+    patch user_path(@user), user: {
+      name: new_name,
+      email: new_email,
+      password: '',
+      password_confirmation: ''
+    }
+    assert_not flash.empty?, 'Flash should not be empty'
+    assert_redirected_to @user, 'Should be redirected to user page'
+    @user.reload
+    assert_equal @user.name, new_name
+    assert_equal @user.email, new_email
+  end
+end
