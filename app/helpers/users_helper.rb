3873c94dce3ce29a0dc93deebeb54d79f41499df
Giniouxe <aurelie.ginioux@gmail.com>
2015-03-31 14:25:34 +0200

    Display all users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,11 @@
 require 'digest/md5'
 
 module UsersHelper
-
-  def gravatar_for(user, options = { size: 80 })
+  def gravatar_for(user, options = { size: 80, html_class: 'gravatar' })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
+    html_class = options[:html_class]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: 'gravatar')
+    image_tag(gravatar_url, alt: user.name, class: html_class)
   end
 end
