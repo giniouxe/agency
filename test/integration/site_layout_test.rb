121e06a1e265555b2b1f3786cd1094917547e3e3
Giniouxe <aurelie.ginioux@gmail.com>
2015-04-01 14:45:37 +0200

    Test layout links when user is logged in

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = Fabricate(:user,
+                      name: 'Foobar', email: 'foobar@example.com',
+                      password: 'foobar', password_confirmation: 'foobar')
+  end
+
   test 'layout links' do
     get root_path
     assert_template 'pages/home'
@@ -9,5 +15,17 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 
     get signup_path
     assert_select 'title', full_title('Sign Up')
+  end
+
+  test 'layout links when logged in' do
+    log_in_as(@user, password: 'foobar')
+    get root_path
+    assert_template 'pages/home'
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', contact_path
+    assert_select 'a[href=?]', users_path
+    assert_select 'a[href=?]', user_path(@user)
+    assert_select 'a[href=?]', edit_user_path(@user)
+    assert_select 'a[href=?]', logout_path
   end
 end
