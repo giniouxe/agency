db9605cc095e990bb1e758630474155f2e504d06
Giniouxe <aurelie.ginioux@gmail.com>
2015-04-01 12:54:16 +0200

    Refactor before_action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
+  before_action :logged_in_user, except: [:new, :create]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
 
