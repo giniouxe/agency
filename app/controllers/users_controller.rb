1b5678505874dafa5e6b77bdf29894b5bf6d00dd
Giniouxe <aurelie.ginioux@gmail.com>
2015-04-02 19:03:48 +0200

    Test for user activation mailer + Call this mailer when new user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in(@user)
-      flash[:success] = 'Welcome to Agency'
-      redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = 'You can now check your emails to activate your account.'
+      redirect_to root_path
     else
       render 'new'
     end
