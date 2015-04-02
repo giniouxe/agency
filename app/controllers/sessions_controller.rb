b7e93ec58314702df0ddde06652981e1386955eb
Giniouxe <aurelie.ginioux@gmail.com>
2015-04-02 21:17:19 +0200

    Log in user only if user is activated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message = 'Your account is not activated.'
+        message << 'Check your emails for the activation link.'
+        flash[:warning] = message
+        redirect_to root_path
+      end
     else
       flash.now[:danger] = 'Sorry! Invalid user/password combination'
       render 'new'
