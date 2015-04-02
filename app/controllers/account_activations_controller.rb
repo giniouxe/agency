1ae75ac7ea0d40622c5836fc415ba974c607dfe6
Giniouxe <aurelie.ginioux@gmail.com>
2015-04-02 21:16:51 +0200

    Activate and login user when activation link is clicked

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,15 @@
 class AccountActivationsController < ApplicationController
+  def edit
+    user = User.find_by(email: params[:email])
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.update_attribute(:activated, true)
+      user.update_attribute(:activated_at, Time.zone.now)
+      log_in(user)
+      flash[:success] = 'Welcome to agency.'
+      redirect_to user
+    else
+      flash[:danger] = 'Invalid activation link.'
+      redirect_to root_path
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -31,9 +31,10 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, nil)
   end
 
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = self.send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   private
