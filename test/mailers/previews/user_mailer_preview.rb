268afe1d320da32a39e51ab1011d9e68fa63eaa1
Giniouxe <aurelie.ginioux@gmail.com>
2015-04-08 17:59:38 +0200

    Display full URL in emails

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -7,7 +7,7 @@ class UserMailerPreview < ActionMailer::Preview
     UserMailer.account_activation(user)
   end
 
-  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/reset_password
+  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
   def password_reset
     user = User.first
     user.reset_token = User.new_token
