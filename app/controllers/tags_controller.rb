1562809523ecaf52bfa6b7b1c937aebfa165a4ea
Giniouxe <aurelie.ginioux@gmail.com>
2015-05-02 00:30:42 +0200

    Paginate articles displayed by tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,6 +2,6 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
     @tags = Tag.all
-    @articles = @tag.articles
+    @articles = @tag.articles.paginate(page: params[:page], per_page: 10)
   end
 end
