3647d309f2fc9c0b218d5221284374e57741f7c2
Giniouxe <aurelie.ginioux@gmail.com>
2015-05-13 17:20:09 +0200

    Seed database with comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,11 +10,8 @@ User.create!(name: 'Simple User', email: 'user@example.com',
   name = Faker::Name.name
   email = "example-#{n + 1}@example.com"
   password = 'password'
-  User.create!(name: name,
-               email: email,
-               password: password,
-               password_confirmation: password,
-               activated: true,
+  User.create!(name: name, email: email, password: password,
+               password_confirmation: password, activated: true,
                activated_at: Time.zone.now)
 end
 
@@ -23,7 +20,7 @@ users = User.all
 20.times do
   title = Faker::Lorem.sentence
   excerpt = Faker::Lorem.paragraph
-  content = Faker::Lorem.paragraph(3)
+  content = Faker::Lorem.paragraphs(5).join(' ')
   tag_list = %w(Foo Bar Fizz Buzz).sample(2).join(', ')
   picture = File.open(Rails.root + "public/uploads/article/picture/#{%w(light coffee wire working writing).sample}.jpg")
   created_at = rand(2.years).seconds.ago
@@ -31,5 +28,15 @@ users = User.all
     user.articles.create!(title: title, excerpt: excerpt, content: content,
                           picture: picture, tag_list: tag_list,
                           created_at: created_at)
+  end
+end
+
+articles = Article.all
+
+5.times do
+  author = Faker::Name.name
+  body = Faker::Lorem.paragraph
+  articles.each do |article|
+    article.comments.create!(author: author, body: body)
   end
 end
