8727986f9125148db3ab7269c5ff87e386ea758f
Giniouxe <aurelie.ginioux@gmail.com>
2015-05-16 10:52:56 +0200

    Change region and add path style

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,8 @@ if Rails.env.production?
       provider:              'AWS',
       aws_access_key_id:     ENV['S3_ACCESS_KEY'],
       aws_secret_access_key: ENV['S3_SECRET_KEY'],
-      region:                'eu-west-1'
+      region:                'eu-central-1',
+      path_style:            true
     }
     config.fog_directory     =  ENV['S3_BUCKET']
   end
